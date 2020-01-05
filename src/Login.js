9249713722b207aa878531591bfb0f8f6889914a
Angelika Kachur <angelika.kachur@gmail.com>
2020-01-05 20:19:19 +0100

    firebase auth

---
+++ src/Login.js
@@ -0,0 +1,78 @@
+import React, { useCallback, useContext } from "react";
+import { withRouter, Redirect } from "react-router";
+import app from "./init.js";
+import * as firebase from 'firebase'
+import { AuthContext } from "./Auth.js";
+
+const Login = ({ history }) => {
+  const handleLogin = useCallback(
+    async event => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await app
+          .auth()
+          .signInWithEmailAndPassword(email.value, password.value);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
+
+  const { currentUser } = useContext(AuthContext);
+  
+  const handleGoogleLogin = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .then(() => {
+        firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(result => {
+          console.log(result)
+          // history.push('/reports')
+          currentUser.setLoggedIn(true)
+        })
+        .catch(error => alert(error))
+      })
+ 
+  }
+
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div>
+      <h1>Log in</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          Email
+          <input name="email" type="email" placeholder="Email" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" placeholder="Password" />
+        </label>
+        <button type="submit">Log in</button>
+
+
+        <button onClick={() => handleGoogleLogin()} className="googleBtn" type="button">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+            alt="logo"
+          />
+          Join With Google
+        </button>
+
+      </form>
+    </div>
+  );
+};
+
+export default withRouter(Login);
