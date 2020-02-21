80384c421d24a5215039e5336371487c84edee4a
Angelika Kachur <angelika.kachur@gmail.com>
2020-02-21 16:42:38 +0200

    init firebase

--- src/Home.js
+++ src/Home.js
@@ -24,6 +24,17 @@ const Home = () => {
     });
   }
 
+  function test() {
+    user.updateProfile({
+      displayName: "Jane Q. User",
+      photoURL: "https://images.unsplash.com/photo-1518796745738-41048802f99a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+    }).then(function() {
+      // Update successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
+  }
+
   
 
   return (
@@ -33,6 +44,27 @@ const Home = () => {
       <h3>Hi, {user.displayName}</h3>
       <img src={user.photoURL} alt="avatar" />
       <button onClick={() => app.auth().signOut()}>Sign out</button>
+   
+      <div className="user-info">
+        <h2>Info Form</h2>
+        <form action="#" className="measurments">
+          <input type="text" placeholder="name" id="info__name" />
+          <button onClick={() => test()}>Send Info</button>
+        </form>
+      </div>
+      
+      <div className="user-measurments">
+        <h2>Measurments Form</h2>
+        <form action="#" className="measurments">
+          <input type="text" placeholder="wheight" id="measurments__wheight" />
+          <input type="text" placeholder="chest" id="measurments__chest" />
+          <input type="text" placeholder="waist" id="measurments__waist" />
+          <input type="text" placeholder="hip" id="measurments__hip" />
+          <button onClick={() => test()}>Send Measurments</button>
+        </form>
+      </div>
+  
+   
     </>
   );
 };
--- src/initFirebase.js
+++ src/initFirebase.js
@@ -1,5 +1,6 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -9,5 +10,30 @@ const app = firebase.initializeApp({
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 });
+
+const db = firebase.firestore();
+var userRef = db.collection("user");
+
+// userRef.doc("Info").set({
+//   fName: "Lika",
+//   lName: "Kachur",
+//   age: 24,
+//   gender: 'Female'
+// });
+
+// userRef.doc("Measurments").set({
+//   date: "date",
+//   wheight: 60,
+//   chest: 90,
+//   waist: 65,
+//   hip: 95
+// });
+
+userRef.get().then((queryShapshot) => {
+  queryShapshot.forEach((doc) => {
+    console.log(`${doc.id}`)
+  })
+})
+
 
 export default app;
